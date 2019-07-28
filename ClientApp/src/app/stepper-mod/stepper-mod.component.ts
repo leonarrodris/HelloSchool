8a2413bad763fd2f95ffa4db17f2fbf5aa524556
Leonardo Jose Rodriguez Camargo <leonarrodris@hotmail.com>
2019-07-28 18:07:35 -0500

    PrimerC
    L

---
+++ ClientApp/src/app/stepper-mod/stepper-mod.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-stepper-mod',
+  templateUrl: './stepper-mod.component.html',
+  styleUrls: ['./stepper-mod.component.css'],
+})
+export class StepperModComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

cadb10e14ed968a4dbf6e74b374ac6a81ccf61da
Leonardo Jose Rodriguez Camargo <leonarrodris@hotmail.com>
2019-07-28 18:45:56 -0500

    L

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -6,10 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HelloSchool.Controllers
 {
+    
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        
+
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
